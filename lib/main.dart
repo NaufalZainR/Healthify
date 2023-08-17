f7d04727b9a1f271cde9c13e20f5e13199d2d4fe
NaufalZainR <naufalzainramadhan53@gmail.com>
2023-08-17 16:10:04 +0700

    membuat fungsi check auth saat membuka aplikasi

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:healtyfy/src/constants/Providers.dart';
 import 'package:healtyfy/src/feature/auth/view/login.dart';
+import 'package:healtyfy/src/feature/auth/view/register.dart';
 import 'package:healtyfy/src/healtyfy.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -9,7 +11,6 @@ import 'firebase_options.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    name: 'Healtyfy',
     options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(const ProviderScope(child: MyApp()));
@@ -25,7 +26,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Healtyfy(),
+      home: const Checker(),
     );
   }
 }
@@ -34,6 +35,19 @@ class Checker extends HookConsumerWidget {
   const Checker({super.key});
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Container();
+    final authCheck = ref.watch(authCheckerProvider);
+
+    return authCheck.when(
+        data: (data) {
+          if(data == null) return const Login();
+          return const Healtyfy();
+        },
+        error: (error, stackTrace) {
+          return Container();
+        },
+        loading: () {
+          return Container();
+        },
+    );
   }
 }
---
+++ lib/src/repository/AuthRepository.dart
@@ -0,0 +1,62 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
+
+import '../healtyfy.dart';
+import '../utils/Snackbar.dart';
+
+class AuthRepository {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final DatabaseReference _dbReference = FirebaseDatabase.instance.ref();
+
+  Stream<User?> get authStateChange => _auth.authStateChanges();
+
+  Future<String> signInWithEmailAndPassword(GlobalKey key, String email, String password) async {
+    try {
+      await _auth.signInWithEmailAndPassword(email: email, password: password);
+
+      final userUid = _auth.currentUser?.uid;
+      final snapshot = await _dbReference.child('users').child(userUid!).get();
+
+      if (snapshot.exists) {
+        User? user = _auth.currentUser;
+        user?.updateDisplayName(snapshot.child('username').value.toString());
+      }
+
+      Navigator.pushReplacement(
+        key.currentState!.context,
+        MaterialPageRoute(builder: (context) => const Healtyfy()),
+      );
+    } on FirebaseAuthException catch (e) {
+      Snackbar.snackbarShow(key.currentContext!, '$e');
+    }
+    return 'success';
+  }
+
+  Future<String> signUpWithEmailAndPassword(GlobalKey key, String email, String password, String username) async {
+    try {
+      UserCredential userCredential = await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      if(userCredential.user != null){
+        await _dbReference.child("users").child(userCredential.user!.uid).set({
+          'username':username,
+          'email':email,
+        });
+
+        User? user = _auth.currentUser;
+        user?.updateDisplayName(username);
+      }
+
+      Navigator.pushReplacement(
+        key.currentState!.context,
+        MaterialPageRoute(builder: (context) => const Healtyfy()),
+      );
+    } on FirebaseAuthException catch (e) {
+      Snackbar.snackbarShow(key.currentContext!, '$e');
+    }
+    return 'success';
+  }
+}
