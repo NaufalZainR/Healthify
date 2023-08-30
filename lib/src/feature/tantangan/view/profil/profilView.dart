da0ec836091550bc77e5373eea37cc893a98b4f3
NaufalZainR <naufalzainramadhan53@gmail.com>
2023-08-30 11:30:35 +0700

    membuat fungsi dan menampilkan rank user login di menu tantangan tab profil

--- lib/src/feature/tantangan/view/profil/profilView.dart
+++ lib/src/feature/tantangan/view/profil/profilView.dart
@@ -17,7 +17,6 @@ class ProfilView extends StatefulHookConsumerWidget {
   @override
   ConsumerState<ConsumerStatefulWidget> createState() => ProfilViewState();
 }
-
 class ProfilViewState extends ConsumerState<ProfilView> {
   GlobalKey lencanaKey = GlobalKey();
   final FirebaseAuth auth = FirebaseAuth.instance;
@@ -26,100 +25,102 @@ class ProfilViewState extends ConsumerState<ProfilView> {
 
   @override
   Widget build(BuildContext context) {
+    GlobalKey profilViewKey = GlobalKey();
+
     return Padding(
       padding: const EdgeInsets.all(15.0),
       child: Column(
         children: [
           StreamBuilder(
-              stream: dbReference
-                  .child('users')
-                  .child(auth.currentUser!.uid)
-                  .onValue,
-              builder: (context, snapshot) {
-                Map<dynamic, dynamic>? data =
-                    snapshot.data?.snapshot.value as Map?;
-                return Expanded(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      ProfilWidget(
-                        peringkat: 1,
-                        score: data?['score'] ?? 0,
-                        username: data?['username'] ?? 'unknown',
-                        photoPath: data?['photo'] ?? '',
-                      ),
-                    ],
-                  ),
+            stream: dbReference.child('users').onValue,
+            builder: (context, snapshot) {
+              ref.read(tugasRepositoryProvider).fetchUserRank(profilViewKey, snapshot);
+              final data = ref.read(tugasRepositoryProvider).listRank;
+              var getIndex = data.indexWhere((element) => element.username == auth.currentUser!.displayName);
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const SizedBox(
+                    width: 80,
+                    height: 80,
+                    child: Center(child: CircularProgressIndicator())
                 );
-              }),
+              }
+              return Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ProfilWidget(
+                      peringkat: getIndex+1,
+                      score: data[getIndex].score,
+                      username: data[getIndex].username,
+                      photoPath: data[getIndex].photoPath,
+                    ),
+                  ],
+                ),
+              );
+            }
+          ),
           StreamBuilder(
-              stream: dbReference.child('list_lencana').onValue,
-              builder: (context, snapshot) {
-                ref
-                    .read(lencanaRepositoryProvider)
-                    .fetchDataLencana(lencanaKey, snapshot);
-                final data = ref.read(lencanaRepositoryProvider).listLencana;
+            stream: dbReference.child('list_lencana').onValue,
+            builder: (context, snapshot) {
+              ref.read(lencanaRepositoryProvider).fetchDataLencana(lencanaKey, snapshot);
+              final data = ref.read(lencanaRepositoryProvider).listLencana;
 
-                ref.read(lencanaRepositoryProvider).userLencana(lencanaKey);
-                final userLencanaList =
-                    ref.read(lencanaRepositoryProvider).userLencanaList;
-                return Expanded(
-                  flex: 2,
-                  child: Column(
-                    children: [
-                      Align(
-                        alignment: Alignment.topLeft,
-                        child: Text(
-                          'Lencana Kamu',
-                          style: GoogleFonts.poppins(
-                              fontSize: 13,
-                              fontWeight: FontWeight.w600,
-                              color: Colors.white),
+              ref.read(lencanaRepositoryProvider).userLencana(lencanaKey);
+              final userLencanaList = ref.read(lencanaRepositoryProvider).userLencanaList;
+              return Expanded(
+                flex: 2,
+                child: Column(
+                  children: [
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: Text(
+                        'Lencana Kamu',
+                        style: GoogleFonts.poppins(
+                          fontSize: 13,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.white
                         ),
                       ),
-                      const SizedBox(
-                        height: 20,
+                    ),
+                    const SizedBox(height: 20,),
+                    GridView.builder(
+                      padding: const EdgeInsets.all(0),
+                      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 3,
+                        crossAxisSpacing: 10,
+                        mainAxisSpacing: 10
                       ),
-                      GridView.builder(
-                        padding: const EdgeInsets.all(0),
-                        gridDelegate:
-                            const SliverGridDelegateWithFixedCrossAxisCount(
-                                crossAxisCount: 3,
-                                crossAxisSpacing: 10,
-                                mainAxisSpacing: 10),
-                        itemBuilder: (context, index) {
-                          if (userLencanaList.contains(data[index].id)) {
-                            return CustomGridBoxWidget(
+                      itemBuilder: (context, index) {
+                        if (userLencanaList.contains(data[index].id)) {
+                          return CustomGridBoxWidget(
                               namaLencana: data[index].titleText,
                               imagePath: data[index].photoPath,
-                            );
-                          }
-                          return Container(
-                            padding: const EdgeInsets.all(20),
-                            decoration: const BoxDecoration(
-                                color: Colors.white,
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(13))),
-                            child: Container(
-                              decoration: const BoxDecoration(
-                                  color: Color(AppColors.bgSoftGrey),
-                                  borderRadius:
-                                      BorderRadius.all(Radius.circular(100))),
-                              child: Icon(
-                                MdiIcons.lock,
-                                color: const Color(AppColors.bgGrey),
-                              ),
-                            ),
                           );
-                        },
-                        itemCount: data.length,
-                        scrollDirection: Axis.vertical,
-                        shrinkWrap: true,
-                      )
-                    ],
-                  ),
-                );
-              }),
+                        }
+                        return Container(
+                          padding: const EdgeInsets.all(20),
+                          decoration: const BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.all(Radius.circular(13))
+                          ),
+                          child: Container(
+                            decoration: const BoxDecoration(
+                              color: Color(AppColors.bgSoftGrey),
+                              borderRadius: BorderRadius.all(Radius.circular(100))
+                            ),
+                            child: Icon(MdiIcons.lock, color: const Color(AppColors.bgGrey),),
+                          ),
+                        );
+                      },
+                      itemCount: data.length,
+                      scrollDirection: Axis.vertical,
+                      shrinkWrap: true,
+                    )
+                  ],
+                ),
+              );
+            }
+          ),
         ],
       ),
     );
