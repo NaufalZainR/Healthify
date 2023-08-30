ec460d0f141aecafb6b2ee07f7b667b0817c54e6
NaufalZainR <naufalzainramadhan53@gmail.com>
2023-08-30 11:47:29 +0700

    fix bug video youtube yang tidak bisa di tampilkan

--- lib/src/feature/edukasi/view/EdukasiDetailView.dart
+++ lib/src/feature/edukasi/view/EdukasiDetailView.dart
@@ -6,30 +6,38 @@ import 'package:webview_flutter/webview_flutter.dart';
 class EdukasiDetailView extends ConsumerStatefulWidget {
   String url;
 
-  EdukasiDetailView({
-    super.key,
-    required this.url
-  });
+  EdukasiDetailView({super.key, required this.url});
 
   @override
-  ConsumerState<ConsumerStatefulWidget> createState() => _EdukasiDetailViewState();
+  ConsumerState<ConsumerStatefulWidget> createState() =>
+      _EdukasiDetailViewState();
 }
 
 class _EdukasiDetailViewState extends ConsumerState<EdukasiDetailView> {
+  late WebViewController webViewController;
+
+  @override
+  void initState() {
+    super.initState();
+    webViewController = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(Colors.white)
+      ..loadRequest(Uri.parse(widget.url));
+  }
 
   @override
   Widget build(BuildContext context) {
-    WebViewController webViewController = WebViewController();
-
     return Scaffold(
-      body: Column(children: [
-        const AppBarBackWidget(),
-        Expanded(
-          child: WebViewWidget(
-            controller: webViewController..loadRequest(Uri.parse(widget.url)),
-          ),
-        )
-      ],),
+      body: Column(
+        children: [
+          const AppBarBackWidget(),
+          Expanded(
+            child: WebViewWidget(
+              controller: webViewController,
+            ),
+          )
+        ],
+      ),
     );
   }
 }
